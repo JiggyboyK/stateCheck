0a568e7f3b08448f4d4306976f4af4440cd708b8
JiggyboyK <korede305@gmail.com>
2022-04-28 14:48:39 +0100

    final check

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import { Component } from 'react';
+import pic from "./theFlash.jpg";
 
 
 
 class App extends Component{
     state = {
-      showState: true,
+      showState: false,
       person:{
       fullName: 'Barry Allen',
       bio:'The Flash',
@@ -14,16 +15,25 @@ class App extends Component{
       profession: 'forensic scientist'
     }
   };
+      clickFunc =()=> {
+        this.setState((prevstate) => ({...prevstate, showState : !prevstate.showState}))  
+      }
 
+  
   render(){
     return (
 
       <div className='App'>
         <h2>Profile</h2>
-        <img src= {this.state.imgSrc} alt='users-profile'  />
-        <p>Fullname: {this.state.person.fullName}</p>
-        <p>Bio: {this.state.person.bio}</p>
-        <p>Professon : {this.state.person.profession}</p>
+        {this.state.showState && (
+        <div>
+          <img src= {pic} className= "barry"  alt='users-profile' />
+          <p>Fullname: {this.state.person.fullName}</p>
+          <p>Biography: {this.state.person.bio}</p>
+          <p>Profession : {this.state.person.profession}</p>
+        </div>
+)} 
+        
 
         <button onClick={this.clickFunc}>Show Profile</button>
       </div>
